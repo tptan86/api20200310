0f558db6a4093dd867c081ce5b6eaa82f6306ac9
Tan Tong Peng <tptan86@gmail.com>
2020-03-11 17:12:23 +0800

    updated workshop02

--- workshop02/local_main.js
+++ workshop02/local_main.js
@@ -18,15 +18,66 @@ app.use(express.urlencoded({ extended: true }));
 
 // Mandatory workshop
 // TODO GET /api/states
-
+app.get('/api/states',
+	(req, resp) => { //handler
+		const result = db.findAllStates();
+		//status code
+		resp.status(200)
+		//set Content-Type
+		resp.type('application/json')
+		resp.set('X-Generated-On', (new Date()).toDateString())
+		resp.json(result)
+	}
+)
 
 // TODO GET /api/state/:state
-
+app.get('/api/state/:state',
+	(req, resp) => { //handler
+		const limit = parseInt(req.query.limit) || 10;
+		const offset = parseInt(req.query.offset) || 0;
+		const result = db.findCitiesByState(req.params.state, {offset, limit});
+		//status code
+		resp.status(200)
+		//set Content-Type
+		resp.type('application/json')
+		resp.set('X-Generated-On', (new Date()).toDateString())
+		resp.json(result)
+	}
+)
 
 // TODO GET /api/city/:cityId
-
+app.get('/api/city/:cityID',
+	(req, resp) => { //handler
+		const result = db.findCityById(req.params.cityID);
+		//status code
+		resp.status(200)
+		//set Content-Type
+		resp.type('application/json')
+		resp.set('X-Generated-On', (new Date()).toDateString())
+		resp.json(result)
+	}
+)
 
 // TODO POST /api/city
+app.post('/api/city',
+	(req, resp) => { //handler
+		const body = req.body
+		if (!db.validateForm(body)) //fail validation
+		{
+			resp.status(400)
+			resp.type('application/json')
+			resp.json({ 'message': 'incomplete form', 'city': 'not sure' })
+			return
+		} 
+
+		//if pass, insert into DB
+		db.insertCity(body)
+		resp.status(201)
+		resp.type('application/json')
+		resp.set('X-Generated-On', (new Date()).toDateString())
+		resp.json({'message': 'created'})
+	}
+)
 
 // Optional workshop
 // TODO HEAD /api/state/:state
@@ -35,10 +86,35 @@ app.use(express.urlencoded({ extended: true }));
 
 
 // TODO GET /state/:state/count
-
+app.get('/api/state/:state/count',
+	(req, resp) => { //handler
+		const count = db.countCitiesInState(req.params.state);
+		const result = {
+			state: req.params.state,
+			numOfCities: count,
+			timestamp: new Date().toDateString()
+		}
+		//status code
+		resp.status(200)
+		//set Content-Type
+		resp.type('application/json')
+		resp.set('X-Generated-On', (new Date()).toDateString())
+		resp.json(result)
+	}
+)
 
 // TODO GET /api/city/:name
-
+app.get('/api/city/:city',
+	(req, resp) => { //handler
+		const result = db.findCitiesByName(req.params.city);
+		//status code
+		resp.status(200)
+		//set Content-Type
+		resp.type('application/json')
+		resp.set('X-Generated-On', (new Date()).toDateString())
+		resp.json(result)
+	}
+)
 
 // End of workshop
 
